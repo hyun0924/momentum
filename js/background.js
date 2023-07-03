ffdb9d64c25460e92f5506d78e1a0a19775cafae
majae <jaehyun668712@gmail.com>
2023-07-03 17:52:04 +0900

    add background

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const backgrounds = ['0.jpg', '1.jpg', '2.jpg'];
+const BG_NUM = backgrounds.length;
+
+const chosenBG = parseInt(Math.random() * BG_NUM);
+
+const bgImage = document.createElement("img");
+bgImage.src = `./img/${chosenBG}.jpg`;
+bgImage.classList.add("background");
